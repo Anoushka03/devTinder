c878058e6cbadf46a1259fdaa87b015856ed78e7
Anoushka03 <03anoushkapalvia@gmail.com>
2025-10-05 18:03:26 +0100

    create multiple route handlers

--- projects/devTinder/src/app.js
+++ projects/devTinder/src/app.js
@@ -2,6 +2,62 @@ const express = require("express");
 
 const app = express();
 
+
+// Playing with the routes
+// This route means the char before + can be written n (n > 0) number of times
+// app.get(/user+a/, (req, res) => {
+//     //console.log(req.query);
+//     res.send("get User regex");
+// });
+
+
+// Match to anything in between user and a "/user2345a"
+// app.get(/user.*a$/, (req, res) => {
+//     //console.log(req.query);
+//     res.send("get User regex");
+// });
+
+
+
+
+
+// In this ? means it will match the char before ? and that char can appear 0 or 1 time
+// app.get(/user?a/, (req, res) => {
+//     //console.log(req.query);
+//     res.send("get User regex");
+// });
+
+
+
+
+
+
+
+
+
+
+
+// It matches exactly with userba and () is capturing group so it will appear in req.params
+// app.get(/user(b)a/, (req, res) => {
+//     console.log(req.params);
+//     res.send("get User regex");
+// });
+
+
+
+
+
+
+
+
+
+
+// it match any path that contains a
+app.get(/a/, (req, res) => {
+    console.log(req.params);
+    res.send("get User regex");
+});
+
 // Order of the request handlers matter because it starts checking from top to bottom and matched with the first
 // matching handler
 // if we keep "/" at the top then all the request will match this and no other routing will happen
