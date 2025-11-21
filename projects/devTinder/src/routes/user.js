38522334ad9019a61033bb4260626696bf157145
Anoushka03 <03anoushkapalvia@gmail.com>
2025-11-21 22:22:55 +0000

    Pagination and feed api

--- projects/devTinder/src/routes/user.js
+++ projects/devTinder/src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middleware/userAuth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_DATA_SAFE = "firstName lastName emailId about";
 
@@ -54,6 +55,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
         res.json({
             data: onlyConnectedUserData
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Error: " + error.message
+        });
+    }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    const lmt = parseInt(req.query.limit) || 10;
+
+
+    // In the feed we will not show those users who are already in connection with the loggedInUser or have sent or
+    // the loggedInUser have sent connection request 
+    // connection can be - accepted, interested, rejected, ignored
+
+    try {
+        const connectionRequests = await ConnectionRequest.find({
+            $or:[
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).populate("fromUserId", "firstName")
+        .populate("toUserId", "firstName");
+
+        const hiddenUsers = new Set();
+        connectionRequests.forEach(req => {
+            hiddenUsers.add(req.fromUserId._id);
+            hiddenUsers.add(req.toUserId._id);
+        });
+        const feedUsers = await User.find({
+            _id: { $nin: Array.from(hiddenUsers) }
+        }).select("firstName").skip((page - 1) * lmt).limit(lmt);
+
+        res.json({
+            data: feedUsers
         });
     } catch (error) {
         res.status(400).json({
